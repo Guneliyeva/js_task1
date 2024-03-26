e7da04c15b856fb512bb4ae0b20bc404918899bc
guneliyeva <guneliyeva0214@gmail.com>
2024-03-26 19:33:32 +0400

    Task downloaded

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ console.log(ad.length);
 // 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
 let word = prompt("Bir soz daxil edin: ");
 let symbol = prompt("Bir simvol daxil edin: ");
-console.log(word.indexOf(symbol));
+console.log(word.includes(symbol));
 
 // // 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
 console.log(word.startsWith(symbol));
@@ -18,9 +18,8 @@ console.log(word.endsWith(symbol));
 // 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
 console.log(word.indexOf(symbol));
 
-
 // 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
-if (word.toUpperCase() == word) {
+if (word == word.toUpperCase()) {
   console.log("True");
 } 
 else {
