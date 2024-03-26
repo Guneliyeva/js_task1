bfbff031e274c4b01e2d4ce5e5cb832ed79acce9
guneliyeva <guneliyeva0214@gmail.com>
2024-03-26 12:45:27 +0400

    task downloaded

---
+++ script.js
@@ -0,0 +1,28 @@
+// Ev taski:
+
+// 1.İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.
+let ad = prompt("Ad daxil edin: ");
+console.log(ad.length);
+
+// 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
+let word = prompt("Bir soz daxil edin: ");
+let symbol = prompt("Bir simvol daxil edin: "); 
+console.log(word.indexOf(symbol));
+
+// 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+console.log(word.startsWith(symbol));
+
+// 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+console.log(word.endsWith(symbol));
+
+// 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
+symbol=parseInt(symbol)
+console.log(word.charAt(symbol));
+
+// 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
+if (word.toUpperCase()==word){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
