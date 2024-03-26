2893963a17725929415061680969aae3e19bed1f
guneliyeva <guneliyeva0214@gmail.com>
2024-03-26 13:50:46 +0400

    task modified

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ console.log(ad.length);
 
 // 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
 let word = prompt("Bir soz daxil edin: ");
-let symbol = prompt("Bir simvol daxil edin: "); 
+let symbol = prompt("Bir simvol daxil edin: ");
 console.log(word.indexOf(symbol));
 
-// 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+// // 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
 console.log(word.startsWith(symbol));
 
-// 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+// // 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
 console.log(word.endsWith(symbol));
 
 // 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
-symbol=parseInt(symbol)
-console.log(word.charAt(symbol));
+console.log(word.indexOf(symbol));
+
 
 // 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
-if (word.toUpperCase()==word){
-    console.log("True");
-}
-else{
-    console.log("False");
+if (word.toUpperCase() == wordCeyran) {
+  console.log("True");
+} 
+else {
+  console.log("False");
 }
